3c8323245b83a2b139591d290203ddfe6778a9d4
juanse77 <juanseraar@hotmail.com>
2022-10-04 19:38:42 +0000

    add docstring tests

--- modeltools/preprocessing.py
+++ modeltools/preprocessing.py
@@ -5,4 +5,21 @@ import numpy as np
 
 
 def get_numerical_features(df):
+    """
+    get_numerical_features devuelve una lista con el nomabre de las columnas que contienen datos de tipo numérico
+    
+    Parameters
+    ----------
+    df: dataframe
+
+    Examples
+    --------
+
+    >>> from modeltools.preprocessing import get_numerical_features
+    >>> import pandas as pd
+    >>> df = pd.DataFrame({"a": [1]})
+    >>> get_numerical_features(df)
+    ['a']
+
+    """
     return list(df.select_dtypes(include=[np.number]).columns)

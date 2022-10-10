114c32eda50dff3c87dbb0f05ec79403c8f1f598
Juan Ramirez <juanseraar@hotmail.com>
2022-10-10 16:25:19 +0000

    pre-commit installed

--- modeltools/preprocessing.py
+++ modeltools/preprocessing.py
@@ -3,6 +3,7 @@ Este módulo contiene helpers para el preprocesamiento de datos
 """
 import numpy as np
 
+
 def dummy(a, b):
     """
     Sums two numbers
@@ -19,19 +20,20 @@ def dummy(a, b):
     """
     return a + b
 
+
 def get_numerical_features(df):
     """
     get_numerical_features devuelve una lista con el nomabre de las columnas que contienen datos de tipo numérico
-    
+
     :param df: DataDrame
     :type df: DataFrame
-    :returns: A list of column names of number datatypes 
+    :returns: A list of column names of number datatypes
     :rtype: List[str]
 
-    #>>> from modeltools.preprocessing import get_numerical_features
-    #>>> import pandas as pd
-    #>>> df = pd.DataFrame({"a": [1]})
-    #>>> get_numerical_features(df)
-    #['a']
+    >>> from modeltools.preprocessing import get_numerical_features
+    >>> import pandas as pd
+    >>> df = pd.DataFrame({"a": [1]})
+    >>> get_numerical_features(df)
+    ['a']
     """
     return list(df.select_dtypes(include=[np.number]).columns)
--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,25 +1,21 @@
 import pandas as pd
 from modeltools.preprocessing import get_numerical_features
 
+
 def test_get_numerical_features_simple():
     """
     En este test vamos a probar que distingue los campos numéricos entre cadenas de texto y campos de otro tipo
     """
 
-    df = pd.DataFrame({
-        "numerica": [5],
-        "categorica": ["rojo"]
-    })
+    df = pd.DataFrame({"numerica": [5], "categorica": ["rojo"]})
 
     # assert es "como un if" que nos falla si la condición es falsa
     assert get_numerical_features(df) == ["numerica"]
 
+
 def test_get_numerical_features_empty():
     """Este test comprueba que se devuelve una lista vacía si no hay ninguna variable num"""
-    df = pd.DataFrame({
-        "categorica1": ["azul"],
-        "categorica2": ["rojo"]
-    })
+    df = pd.DataFrame({"categorica1": ["azul"], "categorica2": ["rojo"]})
 
     assert get_numerical_features(df) == []
 
@@ -34,29 +30,27 @@ def test_get_numerical_features_zero_rows():
     """Este test comprueba que se devuelve una con una variable si el DF
     tiene una variable numérica pero NINGUNA FILA."""
 
-    assert get_numerical_features(pd.DataFrame({'numerica': pd.Series(dtype=int)}) ) == ["numerica"]
+    assert get_numerical_features(pd.DataFrame({"numerica": pd.Series(dtype=int)})) == [
+        "numerica"
+    ]
 
 
 def test_get_numerical_features_complex():
     """
     Este test comprueba que funciona correctamente con números decimales
     """
-    
-    df = pd.DataFrame({
-        "numerica": [5],
-        "numerica2": [5.2]
-    })
+
+    df = pd.DataFrame({"numerica": [5], "numerica2": [5.2]})
 
     # assert es "como un if" que nos falla si la condición es falsa
     assert get_numerical_features(df) == ["numerica", "numerica2"]
+
 
 def test_get_numerical_features_columns_withoutname():
     """
     Este test comprueba que funciona correctamente cuando hay columnas numéricas sin nombre (Columnas con números/posiciones)
     """
-    df = pd.DataFrame([
-        [1, "a"]
-    ])
+    df = pd.DataFrame([[1, "a"]])
 
     assert get_numerical_features(df) == [0]
 
@@ -65,11 +59,10 @@ def test_get_numerical_features_complex():
     """
     Este test comprueba que se devuelve una lista con los valores de campos complejos
     """
-    df = pd.DataFrame({
-        "complejo": [complex(3, 5)]
-    })
+    df = pd.DataFrame({"complejo": [complex(3, 5)]})
 
     assert get_numerical_features(df) == ["complejo"]
 
-#def test_get_numerical_features_simple2():
+
+# def test_get_numerical_features_simple2():
 #    assert 1 == 2
